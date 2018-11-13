e460d777f331b51a47a10ecb1ed4435de6a922a2
Sunita <you@example.com>
2018-11-13 14:03:55 -0700

    use manifest file to push cf config to pcf

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,34 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+@RestController
+public class EnvController {
+
+    private String port;
+    private String MEMORY_LIMIT;
+    private String CF_INSTANCE_INDEX;
+    private String CF_INSTANCE_ADDR;
+
+    Map<String, String> env = new HashMap();
+
+
+    public EnvController(@Value("${PORT:NOT SET}")  String port, @Value("${MEMORY_LIMIT:NOT SET}") String MEMORY_LIMIT,  @Value("${CF_INSTANCE_INDEX:NOT SET}") String CF_INSTANCE_INDEX, @Value("${CF_INSTANCE_ADDR:NOT SET}")  String CF_INSTANCE_ADDR) {
+        this.port = port;
+        this.MEMORY_LIMIT = MEMORY_LIMIT;
+        this.CF_INSTANCE_INDEX = CF_INSTANCE_INDEX;
+        this.CF_INSTANCE_ADDR = CF_INSTANCE_ADDR;
+    }
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", MEMORY_LIMIT);
+        env.put("CF_INSTANCE_INDEX", CF_INSTANCE_INDEX);
+        env.put("CF_INSTANCE_ADDR", CF_INSTANCE_ADDR);
+        return env;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.jsf.el.WebApplicationContextFacesELResolver;
 
 @RestController
 public class WelcomeController {
 
+    private String hello;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}")String hello) {
+        this.hello = hello;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
     }
 }
 
